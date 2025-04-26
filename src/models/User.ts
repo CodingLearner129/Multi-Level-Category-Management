c0a11284398626586ffb94a8addc2e072bb26fb6
Dhruv Parmar <63842882+CodingLearner129@users.noreply.github.com>
2025-04-26 13:47:28 +0530

    Update User.ts
    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,11 +4,13 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        trim: true,
         unique: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
 }, {
     timestamps: true
